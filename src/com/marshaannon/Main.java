19defc82e83237b774be9f4a18c6e58c079f088f
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-28 13:13:26 -0500

    add  create new video object logic

--- src/com/marshaannon/Main.java
+++ src/com/marshaannon/Main.java
@@ -3,5 +3,9 @@ package com.marshaannon;
 public class Main {
 
     public static void main(String[] args) {
+        var video = new Video();
+        video.setFileName("birthday.mp4");
+        video.setTitle("Happy Birthday Yerrhum!");
+        video.setUser(new User("marsha@gmail.com"));
     }
 }
